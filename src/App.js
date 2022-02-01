f4c74d5963bca2a47d2804f9e7b0b8565b2d6633
Spencer-Lake <spencer.m.lake11@gmail.com>
2022-02-01 14:01:47 -0500

    setting up template

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log("hello world")
   return (
     <div className="App">
+      <div className='navbar'>
+        <div className='nav-cont'>
+          <div className='left'>
+            <ul className='left-cont'>
+              <li>Tops</li>
+              <li>Bottoms</li>
+              <li>Misc</li>
+            </ul>
+          </div>
+          <div className='right'>
+            <ul>
+              <li>Profile</li>
+            </ul>
+          </div>
+        </div>
+      </div>
       
     </div>
   );
